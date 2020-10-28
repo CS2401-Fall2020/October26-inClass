9f951560befbe88e35e858b3e5f682fa328f8441
Dan DeBlasio <dan@dandeblasio.com>
2020-10-28 11:35:13 -0600

    in class 28 Oct

--- src/BSTNode.java
+++ src/BSTNode.java
@@ -47,10 +47,93 @@ public class BSTNode {
   
 
   public BSTNode findMin() {
-    return null;
+    BSTNode current = this;
+    while(current.left!=null){
+      current = current.left;
+    }
+    return current;
   }
   
   public void remove(char inVal) {
+    // base case: is the value in our tree?
+    if(!find(inVal)) return;
+    
+    BSTNode current = this;
+    BSTNode parent = null;
+    
+    while(current.value != inVal) {
+      parent = current;
+      if(inVal < current.value) current = current.left;
+      else current = current.right;
+    }
+    
+    // found the node, and it's parent
+    
+    // if its a leaf, remove it
+    if(current.left==null && current.right==null) {
+      // if root is a leaf... can't remove it
+      if(parent==null) return;
+      
+      // make the parent not point to it, don't know if its left or right
+      if(current == parent.left) {
+        parent.left = null;
+      }else {
+        parent.right = null;
+      }
+    }
+    
+    // if it has one child, re-link
+    else if(current.left==null || current.right==null) {
+      if(parent==null) {
+        // replace the root.....
+        if(current.left==null) {
+          current.value = current.right.value;
+          current.left = current.right.left;
+          current.right = current.right.right;
+        }else {
+          current.value = current.left.value;
+          current.right = current.left.right;
+          current.left = current.left.left;
+        }
+      }
+      
+      // has a right child only
+      else if(current.left==null) {
+        if(current == parent.left) {
+          parent.left = current.right;
+        }else {
+          parent.right = current.right;
+        }
+      }else {
+        if(current == parent.left) {
+          parent.left = current.left;
+        }else {
+          parent.right = current.left;
+        }
+      }
+    }
+    
+    // now we know that it has two children
+    else {
+      // find the smallest on the right & remove it
+      BSTNode min_right = current.right.findMin();
+      remove(min_right.value);
+      
+      if(parent == null) {
+        // replace the root....
+        current.value = min_right.value;
+      }else {
+        // replace the current node with that minimum
+        min_right.left = current.left;
+        min_right.right = current.right;
+        if(current == parent.left) {
+          parent.left = min_right;
+        }else {
+          parent.right = min_right;
+        }
+      }
+      
+    }
     
   }
   
--- src/driver.java
+++ src/driver.java
@@ -22,11 +22,43 @@ public class driver {
       System.out.print(i + ":" + root.depth(i) + ",");
     }
     System.out.println();
-    /*root.remove('a');
+    
+    System.out.println("The minimum character is " + root.findMin().value);
+    System.out.println("The minimum character in right subtree is " + root.right.findMin().value);
+
+    root.remove('a'); // test removing..... leaf
     System.out.println("After remove 'a': ");
     root.print();
-    root.remove('f');
+    root.remove('f'); // test removing.... a node with two children
     System.out.println("After remove 'f': ");
-    root.print();*/
+    root.print();
+    // test removing a node with *one* child
+    root.remove('b');
+    System.out.println("After remove 'b': ");
+    root.print();
+    
+    // test removing root with two children
+    root.remove('d');
+    System.out.println("After remove 'd': ");
+    root.print();
+  
+    // test removing the root with one child
+    root.remove('h');
+    root.remove('e');
+    root.insert('b');
+    System.out.println("After remove 'h' then 'e': ");
+    root.print();
+    
+    // test removing root failing because its a leaf
+    root.remove('b');
+    root.remove('c');
+    root.remove('c');
+    root.remove('c');
+    root.remove('c');
+    root.remove('c');
+    root.remove('c');
+    root.remove('X');
+    System.out.println("After remove 'b' then 'c': ");
+    root.print();
   }
 }
